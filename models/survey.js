c560288a809c2976c471bcc28779aac38cf9f5e5
roger.zeng <qqmi@hotmail.com>
2012-11-21 07:36:42 +0800

    nodejs skeleton

---
+++ models/survey.js
@@ -0,0 +1,47 @@
+﻿
+// TODO: var db = require('../db');
+
+function Survey() {
+
+};
+
+module.exports = Survey;
+
+Survey.get = function(id) {
+    if(!id) return;
+    
+    return survey1;
+};
+
+var survey1 = {
+    id: 1,
+    name: '调查1',
+    
+    questions: [{
+        id: 1,
+        title: '问题1',
+        type: 'radio',
+        items: [1,2],
+    },
+    {
+        id: 2,
+        title: '问题2',
+        type: 'checkbox',
+        items: [1,2],
+    },
+    {
+        id: 3,
+        title: '问题3',
+        type: 'select',
+        items: [1,2],
+    },
+    {
+        id: 4,
+        title: '问题4',
+        type: 'input'
+    }]
+};
+
+survey1.items = {};
+survey1.items[1] = '好';
+survey1.items[2] = '不好';
---
+++ routes/index.js
@@ -0,0 +1,14 @@
+﻿
+var Survey = require('../models/survey');
+
+// routers
+
+exports.index = function(req, res){
+  res.render('index', { title: 'Express' });
+};
+
+exports.survey = function(req, res){
+  var result = Survey.get(req.params.id);
+  console.log(result);
+  res.render('survey', { result: result });
+};
